a99009e5e2cc200a70cbab776df9add64000dc66
Victoria <vitalina27@gmail.com>
2017-11-13 14:42:25 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // Add your doToElementsInArray() function here:
 
+  function doToElementsInArray(array, callback) {
+  return   array.forEach(callback);
+  }
+  //evens.forEach(callback);
 
 // Add your changeCompletely() function here:
+function changeCompletely(element, index, array){
+ return array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+
+}
+let arr = ["meat", "fish", "apple"]
+
+ doToElementsInArray(arr, changeCompletely)
+ console.log(arr);
